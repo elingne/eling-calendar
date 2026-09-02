fab1d61a6f3710f26de27a21fbab1b26ad876dc7
elingne <ielingkk@naver.com>
2026-09-02 21:46:57 +0900

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://dxvgbbqsgnmuhieqsuyg.supabase.c";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_LckbCGQxFyB2uB2OvaYEFg_6BTM2G_f";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
